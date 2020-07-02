ddf10046382b295d22475c9a22e6e9c7d38badcd
king <tettehmel@gmail.com>
2020-07-02 17:03:49 -0400

    added input validation

--- src/Components/streams/StreamCreate.js
+++ src/Components/streams/StreamCreate.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
-import { Field, reduxForm, formValues } from "redux-form";
+import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends Component {
-  renderInput({input,label}) {
-    // console.log({input});
+
+  renderError({error,touched}){
+      if(touched && error){
+          return(
+              <div className="ui error message">
+                  <div className="header">{error}</div>
+              </div>
+          )
+      }
+  }
+  renderInput=({input,label, meta})=> {
+    console.log(meta);
+    const className=`field ${meta.error && meta.touched ? 'error':''}`
     return (
-      <div className="field">
+      <div className={className}>
           <label>{label}</label>
-          <input {...input}
-      />
+          <input {...input} autoComplete="off"/>
+          {this.renderError(meta)}
       </div>
     );
   }
@@ -19,7 +30,7 @@ class StreamCreate extends Component {
   render() {
     // console.log(this.props);
     return (
-      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+      <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
         <Field name="title" component={this.renderInput} label="Enter Title"/>
         <Field name="description" component={this.renderInput} label="Enter Description"/>
 
