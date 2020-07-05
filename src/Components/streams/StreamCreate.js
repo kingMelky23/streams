4257e40a0dc6b7c4b0f70630ff508f9c8054e049
king <tettehmel@gmail.com>
2020-07-04 20:49:19 -0400

    feature:api request

--- src/Components/streams/StreamCreate.js
+++ src/Components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
+import {connect } from 'react-redux';
+import {createStream}from '../../actions'
 
 class StreamCreate extends Component {
 
@@ -13,7 +15,7 @@ class StreamCreate extends Component {
       }
   }
   renderInput=({input,label, meta})=> {
-    console.log(meta);
+    // console.log(meta);
     const className=`field ${meta.error && meta.touched ? 'error':''}`
     return (
       <div className={className}>
@@ -23,8 +25,8 @@ class StreamCreate extends Component {
       </div>
     );
   }
-  onSubmit(formValues){
-    console.log(formValues)
+  onSubmit=(formValues)=>{
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -53,7 +55,9 @@ const validate = (formValues)=>{
     return errors;
 }
 
-export default reduxForm({
+const formWrapped= reduxForm({
   form: "streamCreate",
   validate,
 })(StreamCreate);
+
+export default connect(null,{createStream})(formWrapped);
