7966c714f0265d26fdd5a5c81e4ac6e798f20e0c
king <tettehmel@gmail.com>
2020-08-04 09:49:21 -0400

    feature: login, display streams

--- src/Components/streams/StreamList.js
+++ src/Components/streams/StreamList.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import {fetchStreams} from '../../actions'
 
-function StreamList() {
+class StreamList extends Component{
+    componentDidMount(){
+        this.props.fetchStreams()
+    }
+
+    renderList(){
+        return this.props.streams.map(stream=>{
+            return(
+                <div className='item' key={stream.id}>
+                    <i className="large middle aligned icon camera"/>
+                    <div className="conent">
+                        {stream.title}
+                        <div className="description">
+                        {stream.description}
+                        </div>
+                    </div>
+
+                </div> 
+            )
+        })
+    }
+
+    render(){
+         console.log(this.props.streams)
     return (
         <div>
-            StreamList
+            <h2>Streams</h2>
+            <div className="ui celled list">{this.renderList()}</div>
         </div>
     )
+}}
+
+const mapStateToProps=(state)=>{
+    return{streams: Object.values(state.streams)}
 }
 
-export default StreamList;
+export default connect(mapStateToProps,{fetchStreams})(StreamList);
